5056c23d972914e25466f2b640a190b22ea2853d
Lucas Oliveira <lcsd.lucas@gmail.com>
2020-03-26 12:31:12 -0300

    finalizando back-end

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const OngsController = require('./controller/OngsController');
+const IncidentsController = require('./controller/IncidentsController');
+const ProfileController = require('./controller/ProfileController');
+const SessionController = require('./controller/SessionController');
 
 const routes = express.Router();
 
+routes.post('/session', SessionController.store);
+
 routes.get('/ongs', OngsController.index);
 routes.post('/ongs', OngsController.store);
+
+routes.get('/incidents', IncidentsController.index);
+routes.post('/incidents', IncidentsController.store);
+routes.delete('/incidents/:id', IncidentsController.delete);
+
+routes.get('/profile', ProfileController.index);
 
 module.exports = routes;
